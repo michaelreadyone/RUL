59441fb9aa34116c21b245b08e66b1140c827bd7
mike <michael56653556@gmail.com>
2025-05-29 12:09:22 -0400

    first level code clean

---
+++ note.md
@@ -0,0 +1,11 @@
+# Re-org repo
+
+## step 1: Modularize Transformer - CALCE
+
+- move model part to model.py
+- move helper function to utils.py
+- add Rated_Capacity and K to train input
+
+## step 2: Generalize code to use on other format of cell data
+
+- get_train_test function should take data frame as input
---
+++ utils.py
@@ -0,0 +1,74 @@
+import os
+import random
+import numpy as np
+from math import sqrt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import torch
+
+
+def drop_outlier(array,count,bins):
+    index = []
+    range_ = np.arange(1,count,bins)
+    for i in range_[:-1]:
+        array_lim = array[i:i+bins]
+        sigma = np.std(array_lim)
+        mean = np.mean(array_lim)
+        th_max,th_min = mean + sigma*2, mean - sigma*2
+        idx = np.where((array_lim < th_max) & (array_lim > th_min))
+        idx = idx[0] + i
+        index.extend(list(idx))
+    return np.array(index)
+
+def build_sequences(text, window_size):
+    #text:list of capacity
+    x, y = [],[]
+    for i in range(len(text) - window_size):
+        sequence = text[i:i+window_size]
+        target = text[i+window_size]
+
+        x.append(sequence)
+        y.append(target)
+        
+    return np.array(x), np.array(y)
+
+# leave-one-out evaluation: one battery is sampled randomly; the remainder are used for training.
+def get_train_test(data_dict, name, window_size=8):
+    data_sequence=data_dict[name]['capacity']
+    train_data, test_data = data_sequence[:window_size+1], data_sequence[window_size+1:]
+    # why add test data?
+    train_x, train_y = build_sequences(text=train_data, window_size=window_size)
+    for k, v in data_dict.items():
+        if k != name:
+            data_x, data_y = build_sequences(text=v['capacity'], window_size=window_size)
+            train_x, train_y = np.r_[train_x, data_x], np.r_[train_y, data_y]
+            
+    return train_x, train_y, list(train_data), list(test_data)
+
+def relative_error(y_test, y_predict, threshold):
+    true_re, pred_re = len(y_test), 0
+    for i in range(len(y_test)-1):
+        if y_test[i] <= threshold >= y_test[i+1]:
+            true_re = i - 1
+            break
+    for i in range(len(y_predict)-1):
+        if y_predict[i] <= threshold:
+            pred_re = i - 1
+            break
+    return abs(true_re - pred_re)/true_re if abs(true_re - pred_re)/true_re<=1 else 1
+
+def evaluation(y_test, y_predict):
+    mse = mean_squared_error(y_test, y_predict)
+    rmse = sqrt(mean_squared_error(y_test, y_predict))
+    return rmse
+
+
+def setup_seed(seed):
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed) 
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
